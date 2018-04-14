a2452bf7e32e1fbc944aca8b1d70e32420ecf53e
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-04-14 15:20:16 +0300

    added menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,12 +7,15 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+       // System.out.println(new App().getGreeting());
 
-//        TODO: Ты быстро переключился к данным но не решил проблему с меню. Давай по заданию разберись с меню и выбери классы для него, уже потом данные.
+//        TODO: Ты быстро переключился к данным но не решил проблему с меню.
+// Давай по заданию разберись с меню и выбери классы для него, уже потом данные.
         MainMenu MenuObject = new MainMenu();
-        Person person1= new Person("ilya",1999,"kulakov");
-        person1.addPerson("vas", 1222, "asa");
+       // System.out.println( MenuObject);
+        MenuObject.interactWithMenu();
+       // Person person1= new Person("ilya",1999,"kulakov");
+       // person1.addPerson("vas", 1222, "asa");
 
     }
 
--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MainMenu extends AbstractMenu {
 
     public MainMenu() {
@@ -12,9 +13,36 @@ public class MainMenu extends AbstractMenu {
     }
 
     //    TODO: Почему не реализовал?
+
     @Override
     public void interactWithMenu() {
 
+        Scanner sc1 = new Scanner(System.in);
+            int create=1; boolean exit=false;
 
-    }
-}
+            do{ printMenu(); //вызываю печать меню от AbstractMenu
+                System.out.println(" - Ввод пункта меню с клавиатуры: ");
+
+                int command = sc1.nextInt();//
+                switch (command) {
+                    case 1:
+                        System.out.println("Вы ввели число 1");
+                        exit=true;
+                        break;
+                    case 2:
+                        System.out.println("Вы ввели число 2");
+                        exit=true;
+                        break;
+                    case 3:
+                        System.out.println("Вы ввели число 3");
+                        exit=true;
+                        break;
+                    default:
+                        System.out.println("Ошибка! Повторите выбор меню: ");
+                        exit=false;
+                        break;
+                }//switch
+            }while (exit!=true);
+            //do while
+    }//interactWithMenu(
+}//public class MainMenu extends AbstractMenu
